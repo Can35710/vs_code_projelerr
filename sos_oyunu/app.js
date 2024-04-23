f5cf7f6361ec93086ca5685ae8b19ba23ce92e2b
Can35710 <134143225+Can35710@users.noreply.github.com>
2024-04-24 00:19:19 +0300

    app.js

--- sos_oyunu/app.js
+++ sos_oyunu/app.js
@@ -1,44 +1,137 @@
-body {
-    font-family: Arial, Helvetica, sans-serif;
-    text-align: center;
+// Oyun tahtasını temsil eden bir matris tanımlanır.
+// Her bir hücre başlangıçta boş bir karakterle (' ') doldurulur.
+var board;
+
+// "O" sembolünü temsil eden oyuncu değişkeni.
+var playerO = "O";
+
+// "X" sembolünü temsil eden oyuncu değişkeni.
+var playerX = "X";
+
+// Mevcut oyuncuyu belirlemek için kullanılan değişken. Başlangıçta "O" oyuncusuyla başlar.
+var currPlayer = playerO;
+
+// Oyunun bitip bitmediğini belirten bayrak.
+var gameOver = false;
+
+// Sayfa yüklendiğinde oyun tahtasını ayarlayan fonksiyon.
+window.onload = function() {
+    setGame();
 }
 
-hr {
-    width: 500px;
-    height: 2px;
-    background-color: black;
+// Oyun tahtasını oluşturan fonksiyon.
+function setGame() {
+    // 3x3'lük bir matris oluşturulur ve boşluk karakterleriyle doldurulur.
+    board = [
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+    ];
+
+    // Oyun tahtasının HTML temsili oluşturulur.
+    // Her bir hücre (kutu) oluşturulur, CSS sınıfları eklenir ve tıklanabilirlik özelliği eklenir.
+    for (let r = 0; r < 3; r++) {
+        for (let c = 0; c < 3; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            if (r == 0 || r == 1) {
+                tile.classList.add("horizontal-line");
+            }
+            if (c == 0 || c == 1) {
+                tile.classList.add("vertical-line");
+            }
+            tile.innerText = "";
+            tile.addEventListener("click", setTile);
+            document.getElementById("board").appendChild(tile);
+        }
+    }
 }
 
+// Bir hücreye sembol ekleyen ve sırayı değiştiren fonksiyon.
+function setTile() {
+    // Oyun bittiyse işlem yapılmaz.
+    if (gameOver) {
+        return;
+    }
 
-#board {
-    width: 450px;
-    height: 450px;
-    margin: 0 auto;
-    display: flex;
-    flex-wrap: wrap;
-}
+    // Tıklanan hücrenin koordinatları alınır.
+    let coords = this.id.split("-");
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
 
-.tile {
-    width: 147px; /* Kiremitlerin genişliğini belirler */
-    height: 147px; /* Kiremitlerin yüksekliğini belirler */
+    // Seçilen hücre boş değilse işlem yapılmaz.
+    if (board[r][c] != ' ') { 
+        return;
+    }
     
-    /* Metin */
-    font-size: 150px; /* Metin boyutunu belirler */
-    display: flex; /* İçeriği bir satırda hizalar */
-    justify-content: center; /* İçeriği yatay olarak ortalar */
-    align-items: center; /* İçeriği dikey olarak ortalar */   
+    // Seçilen hücreye mevcut oyuncunun sembolü eklenir.
+    board[r][c] = currPlayer; 
+    this.innerText = currPlayer; 
+
+    // Sıra diğer oyuncuya geçer.
+    if (currPlayer == playerO) {
+        currPlayer = playerX;
+    }
+    else {
+        currPlayer = playerO;
+    }
+
+    // Kazanan kontrol edilir.
+    checkWinner();
 }
 
-.winner {
-    background-color: lightgray; /* Kazanan alanın arka plan rengini belirler */
-    color: red; /* Kazanan alanın metin rengini belirler */
-}
+// Kazananı kontrol eden fonksiyon.
+function checkWinner() {
+    // Satırların kontrolü yapılır.
+    for (let r = 0; r < 3; r++) {
+        if (board[r][0] == board[r][1] && board[r][1] == board[r][2] && board[r][0] != ' ') {
+            // Kazanan satır belirlenir ve ilgili hücrelere "winner" sınıfı eklenir.
+            for (let i = 0; i < 3; i++) {
+                let tile = document.getElementById(r.toString() + "-" + i.toString());
+                tile.classList.add("winner");
+            }
+            gameOver = true;
+            return;
+        }
+    }
 
-.horizontal-line {
-    border-bottom: 3px solid black; /* Yatay çizgiyi belirler */
-}
+    // Sütunların kontrolü yapılır.
+    for (let c = 0; c < 3; c++) {
+        if (board[0][c] == board[1][c] && board[1][c] ==  board[2][c] && board[0][c] != ' ') {
+            // Kazanan sütun belirlenir ve ilgili hücrelere "winner" sınıfı eklenir.
+            for (let i = 0; i < 3; i++) {
+                let tile = document.getElementById(i.toString() + "-" + c.toString());                
+                tile.classList.add("winner");
+            }
+            gameOver = true;
+            return;
+        }
+    }
 
-.vertical-line {
-    border-right: 3px solid black; /* Dikey çizgiyi belirler */
-}
+    // Çapraz kontrol yapılır.
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+        // Sol üstten sağ alta doğru olan çapraz için kazanan belirlenir ve ilgili hücrelere "winner" sınıfı eklenir.
+        for (let i = 0; i < 3; i++) {
+            let tile = document.getElementById(i.toString() + "-" + i.toString());                
+            tile.classList.add("winner");
+        }
+        gameOver = true;
+        return;
+    }
 
+    // Diğer çapraz kontrol yapılır.
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
+        // Sağ üstten sol alta doğru olan çapraz için kazanan belirlenir ve ilgili hücrelere "winner" sınıfı eklenir.
+        let tile = document.getElementById("0-2");                
+        tile.classList.add("winner");
+
+        tile = document.getElementById("1-1");                
+        tile.classList.add("winner");
+
+        tile = document.getElementById("2-0");                
+        tile.classList.add("winner");
+        gameOver = true;
+        return;
+    }
+}
